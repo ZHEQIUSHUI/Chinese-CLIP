dccac95f410434a0fce146ca6e8a6e411258f226
zheqiushui <qiushui.zhe@gmail.com>
2023-09-20 11:30:40 +0800

    忘记更新了什么

--- cn_clip/deploy/pytorch_to_onnx.py
+++ cn_clip/deploy/pytorch_to_onnx.py
@@ -13,30 +13,31 @@ from onnxmltools.utils import convert_float_to_float16
 import cn_clip.clip as clip
 from cn_clip.clip.utils import _MODELS, _MODEL_INFO, _download, available_models, create_model, image_transform
 
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--model-arch", 
-        required=True, 
+        "--model-arch",
+        required=True,
         choices=["ViT-B-16", "ViT-L-14", "ViT-L-14-336", "ViT-H-14", "RN50"],
         help="Specify the architecture (model scale) of Chinese-CLIP model to be converted."
     )
     parser.add_argument(
-        "--pytorch-ckpt-path", 
-        default=None, 
-        type=str, 
+        "--pytorch-ckpt-path",
+        default=None,
+        type=str,
         help="Path of the input PyTorch Chinese-CLIP checkpoint. Default to None which will automatically download the pretrained checkpoint."
     )
     parser.add_argument(
-        "--download-root", 
-        default=None, 
-        type=str, 
+        "--download-root",
+        default=None,
+        type=str,
         help="If --pytorch-ckpt-path is None, official pretrained ckpt will be downloaded under --download-root directory and converted. Default to ~/cache/clip/ ."
     )
     parser.add_argument(
-        "--save-onnx-path", 
+        "--save-onnx-path",
         required=True,
-        type=str, 
+        type=str,
         help="Path (prefix) of the output converted ONNX Chinese-CLIP text or vision model."
     )
     parser.add_argument(
@@ -58,13 +59,13 @@ def parse_args():
 
 def packing_small_onnx_files(onnx_path):
     # packing small files into an extra file
-    save_model(load_model(onnx_path), 
-            onnx_path, 
-            location="{}.extra_file".format(os.path.split(onnx_path)[1]),
-            save_as_external_data=True,
-            all_tensors_to_one_file=True,
-            size_threshold=1024,
-            convert_attribute=True)
+    save_model(load_model(onnx_path),
+               onnx_path,
+               location="{}.extra_file".format(os.path.split(onnx_path)[1]),
+               save_as_external_data=True,
+               all_tensors_to_one_file=True,
+               size_threshold=1024,
+               convert_attribute=True)
     # remove small files
     onnx_dir = os.path.split(onnx_path)[0]
     for key in checkpoint['state_dict']:
@@ -75,7 +76,7 @@ def packing_small_onnx_files(onnx_path):
         if key.startswith('visual'):
             small_file_path = os.path.join(onnx_dir, key)
             if os.path.exists(small_file_path):
-                os.remove(small_file_path)                    
+                os.remove(small_file_path)
     os.system("rm -f {}".format(os.path.join(onnx_dir, "Constant_*_attr__value")))
 
 
@@ -92,15 +93,18 @@ if __name__ == '__main__':
     if os.path.isfile(args.pytorch_ckpt_path):
         input_ckpt_path = args.pytorch_ckpt_path
     elif args.model_arch in _MODELS:
-        input_ckpt_path = _download(_MODELS[args.model_arch], args.download_root or os.path.expanduser("./cache/clip"))
+        input_ckpt_path = _download(
+            _MODELS[args.model_arch], args.download_root or os.path.expanduser("./cache/clip"))
     else:
-        raise RuntimeError(f"Model {args.model_arch} not found; available models = {available_models()}")
+        raise RuntimeError(
+            f"Model {args.model_arch} not found; available models = {available_models()}")
 
     with open(input_ckpt_path, 'rb') as opened_file:
         checkpoint = torch.load(opened_file, map_location="cpu")
 
     # prepare the PyTorch implemented model and restore weights
-    model = create_model(_MODEL_INFO[args.model_arch]['struct'], checkpoint).float().eval()
+    model = create_model(
+        _MODEL_INFO[args.model_arch]['struct'], checkpoint).float().eval()
 
     # prepare empty image and text as input placeholders for ONNX
     resolution = _MODEL_INFO[args.model_arch]['input_resolution']
@@ -113,13 +117,13 @@ if __name__ == '__main__':
         # convert text FP32 ONNX model
         text_fp32_onnx_path = f"{args.save_onnx_path}.txt.fp32.onnx"
         torch.onnx.export(model,
-                    (None, text),
-                    text_fp32_onnx_path,
-                    input_names=['texts'],
-                    output_names=['text_features'],
-                    export_params=True,
-                    opset_version=13,
-                    verbose=True)
+                          (None, text),
+                          text_fp32_onnx_path,
+                          input_names=['texts'],
+                          output_names=['text_features'],
+                          export_params=True,
+                          opset_version=13,
+                          verbose=True)
         # convert text FP16 ONNX model based on the FP32 model
         # text_fp16_onnx_path = f"{args.save_onnx_path}.txt.fp16.onnx"
         # text_fp32_onnx_model = load_model(text_fp32_onnx_path)
@@ -137,14 +141,14 @@ if __name__ == '__main__':
         vision_fp32_onnx_path = f"{args.save_onnx_path}.img.fp32.onnx"
         vision_fp32_onnx_hasextra = False
         torch.onnx.export(model,
-                    (image, None),
-                    vision_fp32_onnx_path,
-                    input_names=['image'],
-                    output_names=['unnorm_image_features'],
-                    export_params=True,
-                    do_constant_folding=False,
-                    opset_version=14,
-                    verbose=True)
+                          (image, None),
+                          vision_fp32_onnx_path,
+                          input_names=['images'],
+                          output_names=['image_features'],
+                          export_params=True,
+                          do_constant_folding=False,
+                          opset_version=14,
+                          verbose=True)
         # for ViT-H-14 FP32 model, make another conversion to deal with the generated small files
         if args.model_arch == "ViT-H-14":
             packing_small_onnx_files(vision_fp32_onnx_path)
@@ -163,9 +167,10 @@ if __name__ == '__main__':
 
     print("Finished PyTorch to ONNX conversion...")
     if args.convert_text:
-        print(f">>> The text FP32 ONNX model is saved at {text_fp32_onnx_path}")
+        print(
+            f">>> The text FP32 ONNX model is saved at {text_fp32_onnx_path}")
         # print(f">>> The text FP16 ONNX model is saved at {text_fp16_onnx_path} with extra file {text_fp16_onnx_path}.extra_file")
     if args.convert_vision:
-        print(f">>> The vision FP32 ONNX model is saved at {vision_fp32_onnx_path}" + \
-            (f" with extra file {vision_fp32_onnx_path}.extra_file" if vision_fp32_onnx_hasextra else ""))
+        print(f">>> The vision FP32 ONNX model is saved at {vision_fp32_onnx_path}" +
+              (f" with extra file {vision_fp32_onnx_path}.extra_file" if vision_fp32_onnx_hasextra else ""))
         # print(f">>> The vision FP16 ONNX model is saved at {vision_fp16_onnx_path} with extra file {vision_fp16_onnx_path}.extra_file")
--- convert.sh
+++ convert.sh
@@ -1,5 +1,5 @@
- python cn_clip/deploy/pytorch_to_onnx.py --convert-text --convert-vision --model-arch RN50 --pytorch-ckpt-path weights/clip_cn_rn50.pt --save-onnx-path ax650/r50
- python cn_clip/deploy/pytorch_to_onnx.py --convert-text --convert-vision --model-arch ViT-B-16 --pytorch-ckpt-path weights/clip_cn_vit-b-16.pt --save-onnx-path ax650/vitb16 
- python cn_clip/deploy/pytorch_to_onnx.py --convert-text --convert-vision --model-arch ViT-L-14 --pytorch-ckpt-path weights/clip_cn_vit-l-14.pt --save-onnx-path ax650/vitl14 
- python cn_clip/deploy/pytorch_to_onnx.py --convert-text --convert-vision --model-arch ViT-L-14-336 --pytorch-ckpt-path weights/clip_cn_vit-l-14-336.pt --save-onnx-path ax650/vitl14-336
+python cn_clip/deploy/pytorch_to_onnx.py --convert-text --convert-vision --model-arch RN50 --pytorch-ckpt-path weights/clip_cn_rn50.pt --save-onnx-path ax650/r50
+python cn_clip/deploy/pytorch_to_onnx.py --convert-text --convert-vision --model-arch ViT-B-16 --pytorch-ckpt-path weights/clip_cn_vit-b-16.pt --save-onnx-path ax650/vitb16 
+python cn_clip/deploy/pytorch_to_onnx.py --convert-text --convert-vision --model-arch ViT-L-14 --pytorch-ckpt-path weights/clip_cn_vit-l-14.pt --save-onnx-path ax650/vitl14 
+python cn_clip/deploy/pytorch_to_onnx.py --convert-text --convert-vision --model-arch ViT-L-14-336 --pytorch-ckpt-path weights/clip_cn_vit-l-14-336.pt --save-onnx-path ax650/vitl14-336
 #  python cn_clip/deploy/pytorch_to_onnx.py --convert-text --convert-vision --model-arch ViT-H-14 --pytorch-ckpt-path weights/clip_cn_vit-h-14.pt --save-onnx-path onnx_models/vith14
